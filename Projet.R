bb3f4a6715382121cad5614a92fd3ab179d6a736
zhipengze <82363635+zhipengze@users.noreply.github.com>
2022-06-08 20:35:21 +0200

    Update Projet.R

--- Projet.R
+++ Projet.R
@@ -2,5 +2,15 @@
 #1
 
 data <- read.csv("productivity.csv")
-dim(data)
+str(data)
 colSums(is.na(data))
+withoutNa=na.omit(data)
+attach(withoutNa)
+summary(actual_productivity)
+cor(actual_productivity,team)
+cor(actual_productivity,targeted_productivity)
+cor(actual_productivity,smv)
+cor(actual_productivity,wip)
+cor(actual_productivity,over_time)
+cor(actual_productivity,incentive)
+cor(actual_productivity,idle_time)
